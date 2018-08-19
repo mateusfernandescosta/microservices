beba24773da2818d87b0f4e3d9f3b1a292c85c2d
Mateus Costa <55119@DESKTOP-VS3MAK1>
2018-08-18 21:18:17 -0300

    Feat: Add HATEOAS

--- src/main/java/com/course/microservices/restfulwebservices/Users/UserResource.java
+++ src/main/java/com/course/microservices/restfulwebservices/Users/UserResource.java
@@ -4,6 +4,10 @@ import java.net.URI;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,12 +33,18 @@ public class UserResource {
     }
     
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public Resource<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null){
             throw new UserNotFoundException("id-"+id);
         }
-        return user;
+        
+        //link to resource to get all Users
+        Resource<User> resource = new Resource<>(user);
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+        
+        return resource;
     }
     
     @PostMapping("/users")
